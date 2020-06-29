7499dcd0bce1235b033c517b76f97841cfd295ee
Karlos814 <karolkuziminski1@gmail.com>
2020-06-29 18:49:20 +0200

    13

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationByMail.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationByMail.java
@@ -1,4 +1,9 @@
 package com.kodilla.good.patterns.challenges;
 
-public class InformationByMail {
+public class InformationByMail implements InformationService {
+
+    @Override
+    public void sendMessage(User user) {
+        System.out.println("Wysłanie wiadomości na  adres: "+user.getEmail());
+    }
 }
